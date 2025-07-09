6ec53b417c7c3a65924172cfdb0becb4cb8aba88
Gustavo <guga_eichler@hotmail.com>
2025-07-08 21:59:47 -0300

    Update db engine.

--- app.py
+++ app.py
@@ -15,6 +15,36 @@ import requests
 import json
 
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+# Construct the SQLAlchemy connection string
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+# Create the SQLAlchemy engine
+#engine = create_engine(DATABASE_URL)
+# If using Transaction Pooler or Session Pooler, we want to ensure we disable SQLAlchemy client side pooling -
+# https://docs.sqlalchemy.org/en/20/core/pooling.html#switching-pool-implementations
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
+
+# Test the connection
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Failed to connect: {e}")
 
 API_BASE_URL = st.secrets['API_BASE_URL']
 def login():
@@ -206,7 +236,7 @@ def dashboard():
     # Example for PostgreSQL: "postgresql://user:password@host:port/dbname"
     # Example for SQLite: "sqlite:///path/to/db.sqlite"
     db_url = st.secrets["db_url"]
-    engine = create_engine(db_url)
+    #engine = create_engine(db_url)
 
     # Query the data
     query = "SELECT id_atleta, nome FROM cadastro_atletas"
